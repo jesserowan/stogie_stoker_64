e1712d07f3fac2e73d63a2437df998c0bfa408be
Jesse <jesserowan@gmail.com>
2025-08-04 17:25:21 -0400

    early access build branch

--- Assets/Source/UI/ControlPanelUI.cs
+++ Assets/Source/UI/ControlPanelUI.cs
@@ -16,60 +16,60 @@ public class ControlPanelUI : MonoBehaviour
     private void OnEnable()
     {
         // Debug.Log("Control Panel OnEnable");
-        Time.timeScale = 0;
-        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        // Time.timeScale = 0;
+        // Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
 
-        startOver.onClick.AddListener(StartOver);
-        setDifficulty.onValueChanged.AddListener(SetDifficulty);
-        setBiome.onValueChanged.AddListener(SetBiome);
+        // startOver.onClick.AddListener(StartOver);
+        // setDifficulty.onValueChanged.AddListener(SetDifficulty);
+        // setBiome.onValueChanged.AddListener(SetBiome);
 
         // Debug.Log($"Setting biome and difficulty to: {GameManager.CurrentBiome}, {GameManager.CurrentDifficulty}");
-        setBiome.value = GetBiomeIndex(GameManager.CurrentBiome);
-        setDifficulty.value = GetDifficultyIndex();
+        // setBiome.value = GetBiomeIndex(GameManager.CurrentBiome);
+        // setDifficulty.value = GetDifficultyIndex();
     }
 
     private void OnDisable()
     {
         // Debug.Log("Control Panel OnDisable");
-        Time.timeScale = 1;
-        Cursor.SetCursor(gameCursor, Vector2.zero, CursorMode.Auto);
-        setDifficulty.onValueChanged.RemoveAllListeners();
-        setBiome.onValueChanged.RemoveAllListeners();
-        startOver.onClick.RemoveAllListeners();
+        // Time.timeScale = 1;
+        // Cursor.SetCursor(gameCursor, Vector2.zero, CursorMode.Auto);
+        // setDifficulty.onValueChanged.RemoveAllListeners();
+        // setBiome.onValueChanged.RemoveAllListeners();
+        // startOver.onClick.RemoveAllListeners();
     }
 
     public void SetDifficulty(int difficultyIndex)
     {
         // Debug.Log($"SetDifficulty(): Setting difficulty: {difficulty}");
-        GameManager.CurrentDifficulty = GetDifficulty(difficultyIndex);
+        // GameManager.CurrentDifficulty = GetDifficulty(difficultyIndex);
     }
 
     public void SetBiome(int biome)
     {
         // Debug.Log($"SetBiome(): Setting biome: {biome}");
-        GameManager.CurrentBiome = GetBiome(biome);
+        // GameManager.CurrentBiome = GetBiome(biome);
     }
 
-    public Biome GetBiome(int i) => i switch { 2 => Biome.Country, 1 => Biome.Suburbs, _ => Biome.City };
-    public int GetBiomeIndex(Biome b) => b switch { Biome.Country => 2, Biome.Suburbs => 1, _ => 0 };
+    // public Biome GetBiome(int i) => i switch { 2 => Biome.Country, 1 => Biome.Suburbs, _ => Biome.City };
+    // public int GetBiomeIndex(Biome b) => b switch { Biome.Country => 2, Biome.Suburbs => 1, _ => 0 };
 
-    public DifficultyValue GetDifficulty(int i)
-    {
-        if (i == 2) return DifficultyCatalogue.instance.Hard;
-        if (i == 1) return DifficultyCatalogue.instance.Mid;
-        return DifficultyCatalogue.instance.Easy;
-    }
+    // public DifficultyValue GetDifficulty(int i)
+    // {
+    //     // if (i == 2) return DifficultyCatalogue.instance.Hard;
+    //     // if (i == 1) return DifficultyCatalogue.instance.Mid;
+    //     // return DifficultyCatalogue.instance.Easy;
+    // }
 
-    public int GetDifficultyIndex()
-    {
-        if (GameManager.CurrentDifficulty == DifficultyCatalogue.instance.Hard) return 2;
-        if (GameManager.CurrentDifficulty == DifficultyCatalogue.instance.Mid) return 1;
-        return 0;
-    }
+    // public int GetDifficultyIndex()
+    // {
+        // if (GameManager.CurrentDifficulty == DifficultyCatalogue.instance.Hard) return 2;
+        // if (GameManager.CurrentDifficulty == DifficultyCatalogue.instance.Mid) return 1;
+        // return 0;
+    // }
 
     public void StartOver()
     {
         // Debug.Log("StartOver() called.");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
